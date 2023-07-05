da9a0857fcaa82fcdfa24f80ae8eb907eaf96505
Vódli Aletta <vodlialetta@MacBook-Pro.local>
2023-07-05 22:33:16 +0200

    Crypto Basic Code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+  const [coinList,setCoinList] = useState([]);
+  const [search,setSearch] = useState("");
+
+  useEffect(()=>{
+    axios.get("https://api.coinstats.app/public/v1/coins")
+    .then(res => setCoinList(res.data.coins))
+    .catch(err => console.log(err))
+  },[])
+
   return (
-    <div className="App">
-      <h1>Hello,React</h1>
+
+   
+
+    <div className="crypto-header">
+      <h1>Crypto Search</h1>
+      <input type="text" placeholder='🪙 Bitcoin...' onChange={(e)=>{
+        setSearch(e.target.value)
+      }} />
+
+      <div className='crypto-container'>
+
+      </div>
+
     </div>
   );
 }
